91cdac764339e35091ce2fc9ebd66e41d95f17ca
1seyoung <20171631@edu.hanbat.ac.kr>
2024-12-12 01:21:10 +0900

    Feat : Main 함수, Game 객체 테스트

---
+++ src/main/java/com/practice/Game.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class Game {
+
+
+    public void startGame(){
+
+        System.out.println("Test Game");
+
+    }
+
+    //Game 실행
+
+
+
+    // 요구사항에 맞게 맵 만들기
+
+    // 지뢰 위치 ->
+
+
+    // 사용자 인터랙션 및
+}
--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -1,7 +1,27 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("--------------------------------------");
+        System.out.println("Minesweeper");
+        System.out.println("--------------------------------------");
+        System.out.println("▶︎ Game Start");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        System.out.println("--------------------------------------");
+        System.out.print("Me : ");
+        Scanner input = new Scanner(System.in);
+        if(input.nextInt() == 1) {
+            Game game = new Game();
+            game.startGame();
+        }
+        else if (input.nextInt() == 2){
+            input.close(); // Scanner 닫기
+            System.exit(0); // 시스템 종료
+        }
+
     }
 }
